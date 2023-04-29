9ca2cf131365021dc633391c3b02227c28f12287
vuslatsena <vuslatsena23@gmail.com>
2023-04-29 09:26:44 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function saturdayFun(act = "roller-skate") {
+  return `This Saturday, I want to ${act}!`
+}
+
+function wrapAdjective(wpar = "*") {
+  let inner = function (par = "special") {
+    return `You are ${wpar}${par}${wpar}!`;
+  }
+  return inner;
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+
+wrapAdjective("%")("a dedicated programmer");
